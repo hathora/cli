fa018655e7369ac722d4d45bca6b09fffa708815
George Price <gwprice@mac.com>
2023-05-31 16:25:21 -0400

    add error message for email not verified

--- src/commands/room/create.ts
+++ src/commands/room/create.ts
@@ -33,9 +33,12 @@ export const roomCreateCommand: CommandModule<
 			console.log(JSON.stringify(room));
 		} catch (e) {
 			if (e instanceof ResponseError) {
+				const errorMessage = await e.response.text();
 				ERROR_MESSAGES.RESPONSE_ERROR(
 					e.response.status.toString(),
-					e.response.statusText
+					errorMessage.includes("Email not verified")
+						? "Email not verified"
+						: e.response.statusText
 				);
 			}
 			throw e;
