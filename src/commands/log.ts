28bf94706e46a2ba5da35b6d849528eb80fdbc6e
Harsh Pandey <hpandey793@gmail.com>
2023-09-13 15:17:57 -0400

    disable aggregated logs

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -7,12 +7,10 @@ export const logAllCommand: CommandModule<
 	{},
 	{
 		appId: string;
+		processId: string;
 		follow: boolean | undefined;
 		timestamps: boolean | undefined;
 		tailLines: number | undefined;
-		region: Region | undefined;
-		processId: string | undefined;
-		deploymentId: string | undefined;
 		token: string;
 	}
 > = {
@@ -39,55 +37,29 @@ export const logAllCommand: CommandModule<
 			demandOption: false,
 			describe: "Number of lines to return from most recent logs history",
 		},
-		region: {
-			type: "string",
-			demandOption: false,
-			choices: Object.values(Region),
-			describe: "Region",
-		},
 		processId: {
 			type: "string",
-			demandOption: false,
-			describe: "System generated unique identifier to a runtime instance of your game server",
-		},
-		deploymentId: {
-			type: "number",
-			demandOption: false,
-			describe: "System generated id for a deployment. Increments by 1",
+			demandOption: true,
+			describe:
+				"System generated unique identifier to a runtime instance of your game server",
 		},
 		token: {
 			type: "string",
 			demandOption: true,
 			describe: "Hathora developer token (required only for CI environments)",
 		},
-
 	},
 	handler: async (args) => {
 		const client = getLogApiClient(args.token);
 		try {
-			let fn:
-				| typeof client.getLogsForAppRaw
-				| typeof client.getLogsForDeploymentRaw
-				| typeof client.getLogsForProcessRaw =
-				client.getLogsForAppRaw.bind(client);
-
-			let request: any = {
+			const request: any = {
 				appId: args.appId,
 				follow: args.follow,
 				timestamps: args.timestamps,
 				tailLines: args.tailLines,
-				region: args.region,
 			};
-			if (args.processId !== undefined) {
-				fn = client.getLogsForProcessRaw.bind(client);
-				request.processId = args.processId;
-			} else if (args.deploymentId !== undefined) {
-				fn = client.getLogsForDeploymentRaw.bind(client);
-				request.deploymentId = args.deploymentId;
-			}
-			let respone = await fn(request);
-
-			let body = respone.raw.body!;
+			const respone = await client.getLogsForProcessRaw(request);
+			const body = respone.raw.body!;
 			body["pipe"](process.stdout);
 		} catch (e) {
 			if (e instanceof ResponseError) {
